1f3acdbd1c1ff7c1974ec0e04c9d6160b83c8984
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-02-06 17:58:33 -0500

    charts-added

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useContext(GithubContext);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData}></ExampleChart>; */}
+        <Pie3D data={languages}></Pie3D>;
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
   display: grid;
+
   justify-items: center;
   gap: 2rem;
   @media (min-width: 800px) {
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr;
   }
 
   @media (min-width: 1200px) {
